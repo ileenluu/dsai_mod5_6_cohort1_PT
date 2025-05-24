554b484eee1960970a7e45e7d7b430d3a1a04675
Irene Lua <168757045+ileenluu@users.noreply.github.com>
2025-05-24 15:26:38 +0000

    add MetaMask Private key

--- payment_backend.py
+++ payment_backend.py
@@ -1,11 +1,11 @@
 from web3 import Web3
 
 INFURA_URL = os.getenv("INFURA_URL")
-PRIVATE_KEY = os.getenv("PRIVATE_KEY")
+MetaMask_Private = os.getenv("MetaMask_Private")
 
 # Connect to Sepolia
 w3 = Web3(Web3.HTTPProvider(INFURA_URL))
-account = w3.eth.account.from_key(PRIVATE_KEY)
+account = w3.eth.account.from_key(MetaMask_Private)
 
 print("✅ Wallet Address:", account.address)
 print("💰 Balance:", w3.eth.get_balance(account.address) / 1e18, "SepoliaETH")
