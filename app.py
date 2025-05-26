e71e3b85ef284579ff0d0872a0b59d4ff2bfa2fb
Irene Lua <168757045+ileenluu@users.noreply.github.com>
2025-05-26 04:39:10 +0000

    build 'sync ETH balance'

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import google.generativeai as genai
 import os
 import sqlite3
 import datetime
+from web3 import Web3
+from dotenv import load_dotenv
+
+load_dotenv() # <-- this loads .env file variables
 
 gemini_api_key = os.getenv("gemini_api_key")
 
@@ -59,6 +63,22 @@ def gemini_reply():
 def paynow():
     return(render_template("paynow.html"))
 
+
+INFURA_URL = os.getenv("INFURA_URL")
+print(INFURA_URL)
+MetaMask_Private = os.getenv("MetaMask_Private")
+print(MetaMask_Private)
+# w3 = Web3(Web3.HTTPProvider(INFURA_URL))
+# account = w3.eth.account.from_key(MetaMask_Private)
+
+# @app.route("/")
+# def show_balance():
+#     balance_wei = w3.eth.get_balance(account.address)
+#   balance_eth = w3.fromWei(balance_wei, 'ether')
+#   return (
+#       f"✅ Wallet Address: {account.address}<br>"
+#       f"💰 Balance: {balance_eth} SepoliaETH"
+#    )
 
 @app.route('/users', methods=['GET', 'POST'])
 def users():
